8da115796e03579bcbcb58aa1efe2744b7aef4d7
Travis Reeder <treeder@gmail.com>
2017-11-17 07:05:46 -0800

    Fix tag

--- release.sh
+++ release.sh
@@ -4,5 +4,13 @@ docker run --rm -it -v $PWD:/app -w /app treeder/bump --filename $version_file "
 version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
 echo "Version: $version"
 
+tag="$version"
+git add -u
+git commit -m "Ruby FDK: $version release [skip ci]"
+# todo: might make sense to move this into it's own repo so it can have it's own versioning at some point
+git tag -f -a $tag -m "Ruby FDK version $version"
+git push
+git push origin $tag
+
 gem build fdk.gemspec
 gem push fdk-$version.gem
