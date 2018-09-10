df1daa9711cd1a2d9ec29b9d9107645c7ec5c617
Ewan Slater <ewan.slater@gmail.com>
2018-09-10 22:03:33 +0100

    Updated 'hello world' to use new gem.

--- examples/loop.rb
+++ examples/loop.rb
@@ -1,19 +1,19 @@
 # assumes function deployed at least once
-puts `fn update route myapp /fdk-ruby --format default`
+puts `fn update route examples /hello-ruby --format default`
 sleep 1
 puts "Cold"
 5.times do |i|
     start = Time.now
-    puts `echo "dawg #{i}" | fn call myapp /fdk-ruby`
+    puts `echo "dawg #{i}" | fn call examples /hello-ruby`
     puts "time: #{Time.now - start}"
     puts
 end
-puts `fn update route myapp /fdk-ruby --format json`
+puts `fn update route examples /hello-ruby --format json`
 sleep 1
 puts "Hot"
 5.times do |i|
     start = Time.now
-    puts `echo '{"name":"dawg #{i}"}' | fn call myapp /fdk-ruby`
+    puts `echo '{"name":"dawg #{i}"}' | fn call examples /hello-ruby`
     puts "time: #{Time.now - start}"
     puts
 end
