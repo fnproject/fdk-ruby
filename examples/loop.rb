9642408bd81692d13fcb6712783b408a7e36871d
Ewan Slater <ewan.slater@gmail.com>
2018-09-11 16:42:36 +0100

    set content-type in loop.rb

--- examples/loop.rb
+++ examples/loop.rb
@@ -3,17 +3,17 @@ puts `fn update function examples hello --format default`
 sleep 1
 puts "Cold"
 5.times do |i|
-    start = Time.now
-    puts `echo "dawg #{i}" | fn invoke examples hello`
-    puts "time: #{Time.now - start}"
-    puts
+  start = Time.now
+  puts `echo "dawg #{i}" | fn invoke examples hello`
+  puts "time: #{Time.now - start}"
+  puts
 end
 puts `fn update function examples hello --format json`
 sleep 1
 puts "Hot"
 5.times do |i|
-    start = Time.now
-    puts `echo '{"name":"dawg #{i}"}' | fn invoke examples hello`
-    puts "time: #{Time.now - start}"
-    puts
+  start = Time.now
+  puts `echo '{"name":"dawg #{i}"}' | fn invoke examples hello --content-type='application/json'`
+  puts "time: #{Time.now - start}"
+  puts
 end
