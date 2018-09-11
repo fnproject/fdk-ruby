dccef0dddd0d09d87c422205d61966b42361a5b1
Ewan Slater <ewan.slater@gmail.com>
2018-09-11 12:59:57 +0100

    Updated loop example to `invoke` triggers not `call` routes

--- examples/loop.rb
+++ examples/loop.rb
@@ -1,19 +1,19 @@
 # assumes function deployed at least once
-puts `fn update route examples /hello-ruby --format default`
+puts `fn update function examples hello --format default`
 sleep 1
 puts "Cold"
 5.times do |i|
     start = Time.now
-    puts `echo "dawg #{i}" | fn call examples /hello-ruby`
+    puts `echo "dawg #{i}" | fn invoke examples hello`
     puts "time: #{Time.now - start}"
     puts
 end
-puts `fn update route examples /hello-ruby --format json`
+puts `fn update function examples hello --format json`
 sleep 1
 puts "Hot"
 5.times do |i|
     start = Time.now
-    puts `echo '{"name":"dawg #{i}"}' | fn call examples /hello-ruby`
+    puts `echo '{"name":"dawg #{i}"}' | fn invoke examples hello`
     puts "time: #{Time.now - start}"
     puts
 end
