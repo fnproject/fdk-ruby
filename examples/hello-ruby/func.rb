49f33444a8e99ae096161c153f2132d2239d540d
Ewan Slater <ewan.slater@gmail.com>
2018-09-12 11:39:04 +0100

    testing invoke ... --format issue

--- examples/hello-ruby/func.rb
+++ examples/hello-ruby/func.rb
@@ -1,6 +1,8 @@
 require 'fdk'
 
 def myfunction(context:, input:)
+  STDERR.puts("Hoot hoot")
+  STDERR.puts("format: " + ENV['FN_FORMAT'])
   input_value = input.respond_to?(:fetch) ? input.fetch('name') : input
   name = input_value.to_s.strip.empty? ? 'World' : input_value
   { message: "Hello #{name}!" }
