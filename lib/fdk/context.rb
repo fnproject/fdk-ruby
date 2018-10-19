fa658cfa84ef0db001bef8c1097984c77b90f00d
Ewan Slater <ewan.slater@gmail.com>
2018-10-19 11:39:19 +0100

    All style offences fixed

--- lib/fdk/context.rb
+++ lib/fdk/context.rb
@@ -1,7 +1,6 @@
-require 'date'
+require "date"
 
 module FDK
-
   # Config looks up values in the env vars
   class Config
     def [](key)
@@ -9,67 +8,62 @@ module FDK
     end
   end
 
-
+  # Represents inbound HTTP headers
   class InHeaders
-    def initialize (h, key_fn)
-      @headers = h
+    def initialize(headers, key_fn)
+      @headers = headers
       @key_fn = key_fn
-
     end
 
-    def headerKey(key)
-      if @key_fn
-        key = @key_fn.call(key)
-      end
+    def header_key(key)
+      key = @key_fn.call(key) if @key_fn
       key.downcase
     end
 
     def [](key)
-      h = @headers[headerKey(key)]
-      unless h.nil?
-        return h[0]
-      end
-      nil
+      h = @headers[header_key(key)]
+      return h[0] unless h.nil?
     end
 
-    def each (&block)
-      @headers.each &block
+    def each(&block)
+      @headers.each(&block)
     end
   end
 
+  # Represents outbound HTTP headers
   class OutHeaders < InHeaders
-
-    def initialize(h, key_in_fn)
-      super(h, key_in_fn)
+    def initialize(headers, key_in_fn)
+      super(headers, key_in_fn)
     end
-
 
     def []=(key, value)
       if value.is_a? Array
         h = []
-        value.each {|x| h.push(x.to_s)}
-        @headers[headerKey(key)] = h
+        value.each { |x| h.push(x.to_s) }
+        @headers[header_key(key)] = h
       else
-        @headers[headerKey(key)] = [value.to_s]
+        @headers[header_key(key)] = [value.to_s]
       end
     end
 
     def delete(key)
-      @headers.delete headerKey(key)
+      @headers.delete header_key(key)
     end
   end
 
-
+  # Represents the Fn context for a function execution
   class Context
-
     # FN_CALL_ID - a unique ID for each function execution.
     # FN_REQUEST_URL - the full URL for the request (parsing example)
-    # FN_HEADER_$X - the HTTP headers that were set for this request. Replace $X with the upper cased name of the header and replace dashes in the header with underscores.
-    # $X - any configuration values you've set for the Application or the Route. Replace X with the upper cased name of the config variable you set. Ex: minio_secret=secret will be exposed via MINIO_SECRET env var.
+    # FN_HEADER_$X - the HTTP headers that were set for this request.
+    #   Replace $X with the upper cased name of the header
+    #   and replace dashes in the header with underscores.
+    # $X - any configuration values you've set for the Application or the Route.
+    #   Replace X with the upper cased name of the config variable you set.
+    #   e.g. minio_secret=secret will be exposed via MINIO_SECRET env var.
     # FN_APP_NAME - the name of the application that matched this route, eg: myapp
     # FN_METHOD - the HTTP method for the request, eg: GET or POST
     # FN_MEMORY - a number representing the amount of memory available to the call, in MB
-
 
     attr_reader :headers
     attr_reader :response_headers
@@ -80,31 +74,28 @@ module FDK
       @config ||= Config.new
     end
 
-
     def call_id
-      @headers['fn-call-id']
+      @headers["fn-call-id"]
     end
-
 
     def app_id
-      @config['FN_APP_ID']
+      @config["FN_APP_ID"]
     end
 
-
     def fn_id
-      @config['FN_FN_ID']
+      @config["FN_FN_ID"]
     end
 
     def deadline
-      DateTime.iso8601(@headers['fn-deadline'])
+      DateTime.iso8601(@headers["fn-deadline"])
     end
 
     def memory
-      @config['FN_MEMORY'].to_i
+      @config["FN_MEMORY"].to_i
     end
 
     def content_type
-      @headers['content-type']
+      @headers["content-type"]
     end
 
     def http_context
@@ -112,43 +103,33 @@ module FDK
     end
   end
 
-
+  # Represents the context data (inbound && outbound)
+  # for the execution passed as HTTP headers
   class HTTPContext
-
     attr_reader :headers
     attr_reader :response_headers
 
     def initialize(ctx)
-
+      fn_http_h_ = "fn-http-h-"
       @ctx = ctx
-
-
       http_headers = {}
-      ctx.headers.each {|k, v|
-        if k.downcase.start_with?('fn-http-h-')
-          new_key = k['fn-http-h-'.length..k.length]
-          http_headers[new_key] = v
-        end
-      }
-
+      ctx.headers.each do |k, v|
+        http_headers[k.sub(fn_http_h_, "")] = v if k.downcase.start_with?(fn_http_h_)
+      end
       @headers = InHeaders.new(http_headers, nil)
-      @response_headers = OutHeaders.new(ctx.response_headers, lambda {|s| 'fn-http-h-' + s})
+      @response_headers = OutHeaders.new(ctx.response_headers, ->(s) { fn_http_h_ + s })
     end
 
-
     def request_url
-      @ctx.headers['fn-http-request-url']
+      @ctx.headers["fn-http-request-url"]
     end
 
     def method
-      @ctx.headers['fn-http-method']
+      @ctx.headers["fn-http-method"]
     end
-
 
     def status_code=(val)
-      @ctx.response_headers['fn-http-status'] = val.to_i
+      @ctx.response_headers["fn-http-status"] = val.to_i
     end
-
   end
 end
-
