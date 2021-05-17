d0f1f45eb6d39bc6aaa35594b113bb569d40d72e
Sunny Sethi <sunny.sethi@oracle.com>
2021-05-17 23:23:56 +0530

    adding build and runtime ruby docker images

--- build_images.sh
+++ build_images.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 #
 # Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
 #
@@ -15,18 +15,16 @@
 # limitations under the License.
 #
 
-#version_file=lib/fdk/version.rb
-#docker run --rm -it -v $PWD:/app -w /app treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
-#version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
-#echo "Version: $version"
-#
-#tag="$version"
-#git add -u
-#git commit -m "Ruby FDK: $version release [skip ci]"
-## todo: might make sense to move this into it's own repo so it can have it's own versioning at some point
-#git tag -f -a $tag -m "Ruby FDK version $version"
-#git push
-#git push origin $tag
-#
-#gem build fdk.gemspec
-#gem push fdk-$version.gem
+
+set -xe
+
+if [ -z "$1" ];then
+  echo "Please supply ruby version as argument to build image." >> /dev/stderr
+  exit 2
+fi
+
+rubyversion=$1
+
+echo $rubyversion
+#pushd images/build/${rubyversion} && docker build -t fnproject/ruby:${rubyversion}-dev . && popd
+#pushd images/runtime/${rubyversion} && docker build -t fnproject/ruby:${rubyversion} . && popd
--- release_images.sh
+++ release_images.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 #
 # Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
 #
@@ -15,18 +15,12 @@
 # limitations under the License.
 #
 
-#version_file=lib/fdk/version.rb
-#docker run --rm -it -v $PWD:/app -w /app treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
-#version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
-#echo "Version: $version"
-#
-#tag="$version"
-#git add -u
-#git commit -m "Ruby FDK: $version release [skip ci]"
-## todo: might make sense to move this into it's own repo so it can have it's own versioning at some point
-#git tag -f -a $tag -m "Ruby FDK version $version"
-#git push
-#git push origin $tag
-#
-#gem build fdk.gemspec
-#gem push fdk-$version.gem
+
+user="fnproject"
+image="ruby"
+
+runtime27="2.7.3"
+echo $runtime27
+#docker push ${user}/${image}:${runtime27}
+#docker push ${user}/${image}:${runtime27}-dev
+
