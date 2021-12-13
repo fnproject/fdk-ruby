c9e33c11080a3305142f4a188976a5fd00fc4793
Gaurav Saini <gaurav.s.saini@oracle.com>
2021-12-13 12:07:20 +0530

    Started using OL8 as base

--- internal/cache_alpine_linux_images.sh
+++ internal/cache_alpine_linux_images.sh
@@ -6,6 +6,5 @@ set -ex
 # Hence, one may encounter rate limiting issue while accessing the Alpine Linux docker images which are not present in artifactory.
 # In order to resolve the rate limiting issue, the below script helps to pull the Alpine Linux images from docker hub and cache them in artifactory.
 
-docker pull docker-remote.artifactory.oci.oraclecorp.com/alpine:3.12
-docker pull docker-remote.artifactory.oci.oraclecorp.com/ruby:2.7.3-alpine3.12
+docker pull docker-remote.artifactory.oci.oraclecorp.com/oraclelinux:8-slim
 
