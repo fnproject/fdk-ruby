8acd02efe51020b4c63a9e9c14167c4b44dcff08
akhkasat <akhil.k.kasat@oracle.com>
2023-03-10 15:33:45 +0530

    use moby/buildkit image from OCIR and upgrade docker buildx version

--- internal/build-scripts/build_base_images.sh
+++ internal/build-scripts/build_base_images.sh
@@ -6,9 +6,13 @@ ruby --version
 # Create a builder instance
 (
   docker buildx rm builderInstance || true
-  docker buildx create --name builderInstance --driver-opt=image=docker-remote.artifactory.oci.oraclecorp.com/moby/buildkit:buildx-stable-1 --platform linux/amd64,linux/arm64
+  docker buildx create --name builderInstance --driver-opt=image=iad.ocir.io/oraclefunctionsdevelopm/moby/buildkit:buildx-stable-1 --platform linux/amd64,linux/arm64
   docker buildx use builderInstance
 )
+
+#Teamcity uses a very old version of buildx which creates a bad request body. Pushing the images to OCIR gives a 400 bad request error. Hence, use this 
+#script to upgrade the buildx version.
+./internal/build-scripts/update-buildx.sh
 
 ./internal/build-scripts/build_base_image.sh 2.7
 ./internal/build-scripts/build_base_image.sh 3.1
