167ebf1c5cea1fbe9b521ad72e18180dffddde64
Alex Shand <alex.shand@oracle.com>
2024-09-20 14:52:33 +0100

    Add publish to github script and remove files only used by Teamcity

--- internal/build-scripts/build_base_images.sh
+++
@@ -1,7 +0,0 @@
-
-# Build base fdk build and runtime images
-echo "Ruby Version"
-ruby --version
-
-./internal/build-scripts/build_base_image.sh 3.1
-./internal/build-scripts/build_base_image.sh 3.3
--- internal/build-scripts/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-if [ "${LOCAL}" = "true" ] && [ "${RUN_TYPE}" != "release" ]; then
-  # Remove gem file
-  rm -rf fdk-${BUILD_VERSION}.gem
-fi
